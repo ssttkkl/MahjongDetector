0caa80e81f491622c183a2c39daec85943f6a618
ssttkkl <huang.wen.long@hotmail.com>
2025-04-14 22:34:21 +0800

    支持传入路径/bytes

--- python-lib/src/mahjong_detector/__init__.py
+++ python-lib/src/mahjong_detector/__init__.py
@@ -1,10 +1,15 @@
+import os
+from typing import IO, Union
 from PIL import Image
 
 from .preprocess import preprocess_image
 from .predict import CLASS_NAMES, predict
 
 
-def detect_tiles(img: bytes) -> list[str]:
+def detect_tiles(img: Union[str, bytes, os.PathLike[str], os.PathLike[bytes], IO[bytes], Image.Image]) -> list[str]:
+    if not isinstance(img, Image.Image):
+        img = Image.open(img)
+
     img, padding_info = preprocess_image(img)
     detection = predict(img, padding_info)
     detection.sort(key=lambda x: x.x1)
